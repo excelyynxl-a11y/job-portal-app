c19fef9f341c2fdbde7b1975421c2b31dcfe6e13
Excelyynx <excelyynxl@gmail.com>
2026-03-17 01:50:52 +0800

    api routing for savedJob, application and analytics

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,9 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const jobRoutes = require("./routes/jobRoutes");
+const applicationRoutes = require("./routes/applicationRoutes");
+const savedJobsRoutes = require("./routes/savedJobsRoutes");
+const analyticsRoutes = require("./routes/analyticsRoutes");
 
 const app = express();
 
@@ -33,6 +36,9 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/jobs", jobRoutes);
+app.use("/api/application", applicationRoutes);
+app.use("/api/save-jobs", savedJobsRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 // serve upload folders
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
