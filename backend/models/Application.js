9eb592369eddc969ec841acf5f86ec45924aa460
Excelyynx <excelyynxl@gmail.com>
2026-03-16 17:21:46 +0800

    api routing for Job

--- backend/models/Application.js
+++ backend/models/Application.js
@@ -1,10 +1,9 @@
-const { application } = require("express");
 const mongoose = require("mongoose");
 
 const applicationSchema = new mongoose.Schema(
     {
         job: { type: mongoose.Schema.Types.ObjectId, ref: "Job", required: true },
-        applicant: { types: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+        applicant: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
         resume: { type: String },
         status: {
             type: String, 
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ const path = require("path");
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const jobRoutes = require("./routes/jobRoutes");
 
 const app = express();
 
@@ -28,7 +30,9 @@ connectDB();
 app.use(express.json());
 
 // routes 
-// app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/jobs", jobRoutes);
 
 // serve upload folders
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
